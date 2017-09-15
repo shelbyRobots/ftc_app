44a509297bbf8a6e0aab86011ca988f060c30214
shelbyRobots <haithcockfamily@gmail.com>
2017-09-15 11:00:26 -0400

    Add managed gamepad - digital button state. Add initial ability for POV and Car driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTank_Driver.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTank_Driver.java
@@ -1,35 +1,3 @@
-/*
-Copyright (c) 2016 Robert Atkinson
-
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Robert Atkinson nor the names of his contributors may be used to
-endorse or promote products derived from this software without specific prior
-written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
-TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.util.RobotLog;
@@ -42,53 +10,14 @@ import com.qualcomm.robotcore.util.Range;
 //@Disabled
 public class TeleopTank_Driver extends LinearOpMode
 {
-
     @Override
     public void runOpMode() throws InterruptedException
     {
-        double left;                             //driver  left joy
-        double right;                            //driver right joy
-        double shooter;                          //operator right trigger float
-        double bkwr_shooter;
-
-        boolean shoot_pressed;                   //operator right trigger boolean
-        boolean flicker_pressed;                 //driver a button
-        boolean last_shoot_pressed = false;
-        boolean last_flicker_pressed = false;
-        boolean flickertoggle = false;
-        boolean d_down_last = false;             //operator dpad -> adjust shoot power
-        boolean d_up_last = false;
-        boolean b_pressed;                       //driver b button -> brake/float toggle
-        boolean b_pressed_last = false;
-        boolean invert_drive_pressed;            //driver y button -> toggle front/back
-        boolean last_invert_drive_pressed = false;
-        boolean switch_mode_pressed;             //driver x button -> temporary test for motor mode
-        boolean last_switch_mode_pressed = false;
-        boolean bkwr_shoot_pressed;              //with prefix bkwr_, variable is used to run shooters backwards
-        boolean last_bkwr_shoot_pressed = false;
-
-        double lpushed;
-        double rpushed;
-        boolean lpush;                           //driver left trigger -> move lpusher left
-        boolean rpush;                           //driver left trigger -> move lpusher right
-        boolean lpush_last = false;
-        boolean rpush_last = false;
-
-        boolean lbump;                           //operator left bumper -> auto shoot
-        boolean lbump_last = false;
-
         Input_Shaper ishaper = new Input_Shaper();
-
         DcMotor.ZeroPowerBehavior zeroPwr = DcMotor.ZeroPowerBehavior.FLOAT;
-
         double shoot_scale = 0.75;
 
-        double elev;
-        double sweep;
-
-        /* Initialize the hardware variables.
-         * The init() method of the hardware class does all the work here
-         */
+        /* Initialize the hardware variables. */
         robot.init(this);
 
         if (robot.leftMotor  != null &&
@@ -97,13 +26,11 @@ public class TeleopTank_Driver extends LinearOpMode
         {
             robot.leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
             robot.rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            //robot.leftMotor.setMaxSpeed(4000);
-            //robot.rightMotor.setMaxSpeed(4000);
             dtrn.init(robot);
         }
 
         // Send telemetry message to signify robot waiting;
-        telemetry.addData("Say", "Hello Driver");    //
+        telemetry.addData("Say", "Hello Driver");
         telemetry.update();
 
         robot.setDriveDir(ShelbyBot.DriveDir.SWEEPER);
@@ -117,41 +44,69 @@ public class TeleopTank_Driver extends LinearOpMode
         waitForStart();
 
         boolean toggle = false;
-        boolean bkwtoggle = false;
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive())
         {
-            // Run wheels in tank mode
-            // (note: The joystick goes negative when pushed forwards, so negate it)
-            if(gamepad2.dpad_down && !d_down_last)   shoot_scale -= 0.05;
-            else if(gamepad2.dpad_up && !d_up_last)  shoot_scale += 0.05;
+            gpad1.update();
+            gpad2.update();
 
-            shoot_scale = Range.clip(shoot_scale, 0.0, 1.0);
-            d_down_last = gamepad2.dpad_down;
-            d_up_last   = gamepad2.dpad_up;
+            boolean use_auto_shoot = false;
 
-            left  = -gamepad1.left_stick_y;
-            right = -gamepad1.right_stick_y;
-            shooter = gamepad2.right_trigger;
-            bkwr_shooter = gamepad2.left_trigger;
-            lpushed = gamepad1.left_trigger;
-            rpushed = gamepad1.right_trigger;
-            flicker_pressed = gamepad1.a;
+            boolean toggle_run_mode = gpad1.just_pressed(ManagedGampad.Button.X);
+            boolean invert_drive_dir = gpad1.just_pressed(ManagedGampad.Button.Y);
+            boolean toggle_rpusher = gpad1.just_pressed(ManagedGampad.Button.R_TRIGGER);
+            boolean toggle_lpusher = gpad1.just_pressed(ManagedGampad.Button.L_TRIGGER);
+            boolean toggle_float = gpad1.just_pressed(ManagedGampad.Button.B);
+            boolean decr_shoot_scale = gpad2.just_pressed(ManagedGampad.Button.D_DOWN);
+            boolean incr_shoot_scale = gpad2.just_pressed(ManagedGampad.Button.D_UP);
+            boolean toggle_shoot_mtr = gpad2.just_pressed(ManagedGampad.Button.R_TRIGGER);
+            boolean toggle_bshoot_mtr = gpad2.just_pressed(ManagedGampad.Button.L_TRIGGER);
+            boolean auto_shoot = gpad2.just_pressed(ManagedGampad.Button.L_BUMP);
 
-            b_pressed = gamepad1.b;
-
-            left  = ishaper.shape(left);
-            right = ishaper.shape(right);
-            robot.leftMotor.setPower(left);
-            robot.rightMotor.setPower(right);
-            elev  = gamepad2.left_stick_y;
-            sweep = gamepad2.right_stick_y;
+            double shooter = gpad2.value(ManagedGampad.AnalogInput.R_TRIGGER_VAL);
+            double bkwr_shooter = gpad2.value(ManagedGampad.AnalogInput.L_TRIGGER_VAL);
+            double elev = gpad2.value(ManagedGampad.AnalogInput.L_STICK_Y);
+            double sweep = gpad2.value(ManagedGampad.AnalogInput.R_STICK_Y);
             robot.elevMotor.setPower(elev);
             robot.sweepMotor.setPower(sweep);
 
-            shoot_pressed = (shooter > 0);
+            // Run wheels in tank mode
+            // (note: The joystick goes negative when pushed forwards, so negate it)
+            double left = -gpad1.value(ManagedGampad.AnalogInput.L_STICK_Y);
+            double right = -gpad2.value(ManagedGampad.AnalogInput.R_STICK_Y);
+            left = ishaper.shape(left);
+            right = ishaper.shape(right);
 
-            if(shoot_pressed && !last_shoot_pressed)
+            double speed = -right;
+            double turn = gpad1.value(ManagedGampad.AnalogInput.R_STICK_X);
+
+            switch (driveType)
+            {
+                case TANK_DRIVE:
+                    robot.leftMotor.setPower(left);
+                    robot.rightMotor.setPower(right);
+                    break;
+
+                case ARCADE_DRIVE:
+                    robot.leftMotor.setPower(speed + turn);
+                    robot.rightMotor.setPower(speed - turn);
+                    break;
+
+                case CAR_DRIVE:
+                    robot.leftMotor.setPower(((1 - Math.abs(turn))  * speed +
+                                              (1 - Math.abs(speed)) * turn  +
+                                              turn + speed) / 2);
+                    robot.rightMotor.setPower(((1 - Math.abs(turn))  * speed -
+                                               (1 - Math.abs(speed)) * turn  -
+                                               turn + speed) / 2);
+                    break;
+            }
+
+            if(decr_shoot_scale)      shoot_scale -= 0.05;
+            else if(incr_shoot_scale) shoot_scale += 0.05;
+            shoot_scale = Range.clip(shoot_scale, 0.0, 1.0);
+
+            if(toggle_shoot_mtr)
             {
                 toggle = !toggle;
 
@@ -160,11 +115,8 @@ public class TeleopTank_Driver extends LinearOpMode
                 else
                     shooter_motors(0.0);
             }
-            last_shoot_pressed = shoot_pressed;
 
-            bkwr_shoot_pressed = (bkwr_shooter > 0);
-
-            if(bkwr_shoot_pressed && !last_bkwr_shoot_pressed)
+            if(toggle_bshoot_mtr)
             {
                 toggle = !toggle;
 
@@ -173,11 +125,8 @@ public class TeleopTank_Driver extends LinearOpMode
                 else
                     shooter_motors(0.0);
             }
-            last_bkwr_shoot_pressed = bkwr_shoot_pressed;
 
-            rpush = (rpushed > 0);
-
-            if(rpush && !rpush_last)
+            if(toggle_rpusher)
             {
                 if(curRpushPos == R_UP_PUSH_POS)
                 {
@@ -189,11 +138,8 @@ public class TeleopTank_Driver extends LinearOpMode
                 }
                 robot.rpusher.setPosition(curRpushPos);
             }
-            rpush_last = rpush;
 
-            lpush = (lpushed > 0);
-
-            if(lpush && !lpush_last)
+            if(toggle_lpusher)
             {
                 if(curLpushPos == L_UP_PUSH_POS)
                 {
@@ -205,10 +151,8 @@ public class TeleopTank_Driver extends LinearOpMode
                 }
                 robot.lpusher.setPosition(curLpushPos);
             }
-            lpush_last = lpush;
 
-
-            if(b_pressed && !b_pressed_last)
+            if(toggle_float)
             {
                 if(zeroPwr == DcMotor.ZeroPowerBehavior.BRAKE)
                     zeroPwr = DcMotor.ZeroPowerBehavior.FLOAT;
@@ -217,10 +161,8 @@ public class TeleopTank_Driver extends LinearOpMode
                 robot.leftMotor.setZeroPowerBehavior(zeroPwr);
                 robot.rightMotor.setZeroPowerBehavior(zeroPwr);
             }
-            b_pressed_last = b_pressed;
 
-            lbump = gamepad2.left_bumper;
-            if(lbump && !lbump_last)
+            if(auto_shoot)
             {
                 RobotLog.ii("SJH", "AUTOSHOOT");
                 robot.shotmotor1.setPower(shoot_scale);
@@ -234,46 +176,39 @@ public class TeleopTank_Driver extends LinearOpMode
                 dtrn.stopAndReset();
 
                 RobotLog.ii("SJH", "DONE AUTOSHOOT MOVE");
-//                ElapsedTime stimer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
-//                sleep(500);
-//                robot.sweepMotor.setPower(-1.0);
-//                robot.elevMotor.setPower(-1.0);
-//                sleep(1500);
-//                robot.shotmotor1.setPower(0);
-//                robot.shotmotor2.setPower(0);
-//                robot.sweepMotor.setPower(0);
-//                robot.elevMotor.setPower(0);
-//                RobotLog.ii("SJH", DONE AUTOSHOOT");
+                if(use_auto_shoot)
+                {
+                    sleep(500);
+                    robot.sweepMotor.setPower(-1.0);
+                    robot.elevMotor.setPower(-1.0);
+                    sleep(1500);
+                    robot.shotmotor1.setPower(0);
+                    robot.shotmotor2.setPower(0);
+                    robot.sweepMotor.setPower(0);
+                    robot.elevMotor.setPower(0);
+                    RobotLog.ii("SJH", "DONE AUTOSHOOT");
+                }
             }
-            lbump_last = lbump;
 
-            invert_drive_pressed = gamepad1.y;
-            if(invert_drive_pressed && !last_invert_drive_pressed)
+            if(invert_drive_dir)
             {
                 robot.invertDriveDir();
             }
-            last_invert_drive_pressed = invert_drive_pressed;
 
-            switch_mode_pressed = gamepad1.x;
-            if(switch_mode_pressed && !last_switch_mode_pressed)
+            if(toggle_run_mode)
             {
                 DcMotor.RunMode currMode = robot.leftMotor.getMode();
                 if(currMode == DcMotor.RunMode.RUN_USING_ENCODER)
                 {
                     robot.leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                     robot.rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                    //robot.leftMotor.setMaxSpeed(4000);
-                    //robot.rightMotor.setMaxSpeed(4000);
                 }
                 else
                 {
                     robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                     robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                    //robot.leftMotor.setMaxSpeed(2650);
-                    //robot.rightMotor.setMaxSpeed(2650);
                 }
             }
-            last_switch_mode_pressed = switch_mode_pressed;
 
             telemetry.addData("left : ",  "%.2f", left);
             telemetry.addData("right : ", "%.2f", right);
@@ -281,8 +216,6 @@ public class TeleopTank_Driver extends LinearOpMode
             telemetry.addData("sweep : ",  sweep);
             telemetry.addData("shooters", "%.2f", shooter);
             telemetry.addData("shooters", "%.2f", bkwr_shooter);
-            telemetry.addData("shootpwr", "%s", last_bkwr_shoot_pressed);
-            telemetry.addData("shootpwr", "%s", last_shoot_pressed);
             telemetry.addData("shoot_scale", "%.2f", shoot_scale);
             telemetry.addData("SHT1CNT", "%5d", robot.shotmotor1.getCurrentPosition());
             telemetry.addData("SHT2CNT", "%5d", robot.shotmotor2.getCurrentPosition());
@@ -294,52 +227,20 @@ public class TeleopTank_Driver extends LinearOpMode
         }
     }
 
-
     private void shooter_motors(double speed)
     {
         robot.shotmotor1.setPower(speed);
         robot.shotmotor2.setPower(speed);
     }
 
-    private void bkwr_shooter_motors ()
+    private enum TELEOP_DRIVE_TYPE
     {
-        robot.shotmotor1.setPower(-1.0);
-        robot.shotmotor2.setPower(-1.0);
+        TANK_DRIVE,
+        ARCADE_DRIVE,
+        CAR_DRIVE
     }
 
-    private void do_pushButtonright (ButtonSide bside)
-    {
-        if (bside == ButtonSide.LEFT) {
-            robot.rpusher.setPosition(R_UP_PUSH_POS);
-        }
-
-        if (bside == ButtonSide.RIGHT) {
-            robot.rpusher.setPosition(R_DN_PUSH_POS);
-        }
-    }
-
-    private void do_pushButtonleft (ButtonSide bside)
-    {
-        if (bside == ButtonSide.LEFT) {
-            robot.lpusher.setPosition(L_UP_PUSH_POS);
-        }
-
-        if (bside == ButtonSide.RIGHT) {
-            robot.lpusher.setPosition(L_DN_PUSH_POS);
-        }
-    }
-
-    private enum ButtonSide
-    {
-        UNKNOWN,
-        LEFT,
-        RIGHT
-    }
-
-
-
-    //This line of text has no use. It can be deleted.
-
+    private final static TELEOP_DRIVE_TYPE driveType = TELEOP_DRIVE_TYPE.TANK_DRIVE;
 
     private final static double L_DN_PUSH_POS = 1.0;
     private final static double R_DN_PUSH_POS = 0.05;
@@ -348,4 +249,6 @@ public class TeleopTank_Driver extends LinearOpMode
 
     private ShelbyBot robot = new ShelbyBot();
     private Drivetrain dtrn = new Drivetrain();
+    private ManagedGampad gpad1 = new ManagedGampad(gamepad1);
+    private ManagedGampad gpad2 = new ManagedGampad(gamepad2);
 }
