03409e8c1cad9501981258a4f7c1af32344555a4
RyanA3 <32079801+RyanA3@users.noreply.github.com>
2017-09-30 10:06:03 -0400

    The relic recovery field points. (#1)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/field/RrField.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.field;
+
+
+import org.firstinspires.ftc.teamcode.util.Point2d;
+
+/**
+ * Created by Ryan on 9/27/2017.
+ */
+
+public class RrField extends Field
+{
+    static final Point2d RLBS = new Point2d("RLBS", -48.0, -48.0);
+    static final Point2d RLJB = new Point2d("RLJB", -48.0, -68.0);
+
+    static final Point2d RLCL = new Point2d("RLCL", -4.37, -70.0);
+    static final Point2d RLCC = new Point2d("RLCC", -12.0, -70.0);
+    static final Point2d RLCR = new Point2d("RLCR", -19.63, -70.0);
+
+    static final Point2d RLRR = new Point2d("RLRR", -68.0, -68.0);
+    static final Point2d RRBS = new Point2d("RRBS", 24.0, -48.0);
+    static final Point2d RRJB = new Point2d("RRJB", 24.0, -68.0);
+    static final Point2d RRCL = new Point2d("RRCL", 70.0, -28.37);
+    static final Point2d RRCC = new Point2d("RRCC", 70.0, -36.0);
+    static final Point2d RRCR = new Point2d("RRCR", 70.0, -43.63);
+    static final Point2d RRRR = new Point2d("RRRR", 68.0, -68.0);
+    static final Point2d RARZ = new Point2d("RARZ", -72.0, -12.0);
+}
