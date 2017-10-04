8d6ad09d1974c95659669baf4b70c7b03871ffc3
shelbyRobots <haithcockfamily@gmail.com>
2017-10-04 19:26:18 -0400

    Changed the convertion from BGR to RGB

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/image/MajorColorDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/image/MajorColorDetector.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.image;
 
+import com.qualcomm.robotcore.util.RobotLog;
+
+import org.firstinspires.ftc.teamcode.util.CommonUtil;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfFloat;
@@ -10,29 +13,56 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import hallib.HalDashboard;
+
 /**
  * Created by crazy on 9/27/2017.
  */
 
 public class MajorColorDetector extends Detector {
-    public void logTelemetry(){}
-
-    private static final double THRESHOLD = .25;
+    private static final double THRESHOLD = .5;
     private static final int BINS = 8;
     private static final float MIN_VALUE = 0.0f;
     private static final float MAX_VALUE = 255.0f;
+    private Color foundColor = Color.NONE;
+
+    private double redPct = 0.0;
+    private double bluPct = 0.0;
+
+    private HalDashboard dashboard;
+    private static final String TAG = "SJH_MCD";
+
     public enum Color {
         RED, BLUE, NONE
     }
-    private Color foundColor = Color.NONE;
+
+    public MajorColorDetector()
+    {
+        dashboard = CommonUtil.getInstance().getDashboard();
+    }
+
+    public void logTelemetry()
+    {
+        dashboard.displayPrintf(4,"Color Detected: %s", foundColor);
+        dashboard.displayPrintf(5,"RedPct %4.2f", redPct);
+        dashboard.displayPrintf(6,"BluPct %4.2f", bluPct);
+    }
+
     public void logDebug()
     {
-
+        RobotLog.ii(TAG, "Color Detected %s redPct %4.2f bluPct %4.2f",
+                foundColor, redPct, bluPct);
     }
+
     public void setImage( Mat img )
     {
         super.setImage(img);
         extract();
+    }
+
+    public Color getMajorColor()
+    {
+        return foundColor;
     }
 
     private void extract() {
@@ -40,13 +70,13 @@ public class MajorColorDetector extends Detector {
         Mat histHue = new Mat();
         Mat histSaturation = new Mat();
 
-        Imgproc.cvtColor(showImg, hsvImage, Imgproc.COLOR_BGR2HSV);
+        Imgproc.cvtColor(showImg, hsvImage, Imgproc.COLOR_RGB2HSV);
         List<Mat> channels = new ArrayList<Mat>();
         Core.split(hsvImage, channels);
 
         //Histogram for hue
         Imgproc.calcHist(Arrays.asList( new Mat[]{channels.get(0)} ), new MatOfInt(0),
-                new Mat(), histHue, new MatOfInt(BINS), new MatOfFloat(MIN_VALUE, MAX_VALUE));
+                new Mat(), histHue, new MatOfInt(BINS), new MatOfFloat(MIN_VALUE, 179));
 
         //Histogram for saturation
         Imgproc.calcHist(Arrays.asList( new Mat[]{channels.get(1)} ), new MatOfInt(0),
@@ -55,31 +85,45 @@ public class MajorColorDetector extends Detector {
 
         double sum = Core.sumElems(histHue).val[0];
         double[] values = new double[histHue.height()+histSaturation.height()];
+
+        RobotLog.dd(TAG, "SUM_Hue %f", sum);
+        RobotLog.dd(TAG, "HueMat %d x %d", histHue.width(), histHue.height());
         int k = 0;
         for (int i = 0; i < histHue.height(); ++i ) {
-            values[k++] = histHue.get(i, 0)[0]/sum;
+            values[k] = histHue.get(i, 0)[0]/sum;
+            RobotLog.dd(TAG, "Val_hue %d %f", k, values[k]);
+            k++;
         }
         sum = Core.sumElems(histSaturation).val[0];
+        //RobotLog.dd(TAG, "SUM_Sat %f", sum);
+        //RobotLog.dd(TAG, "SatMat %d x %d", histSaturation.width(), histSaturation.height());
         for ( int i = 0; i < histSaturation.height(); ++i) {
-            values[k++] = histSaturation.get(i, 0)[0]/sum;
+            values[k] = histSaturation.get(i, 0)[0]/sum;
+            //RobotLog.dd(TAG, "Val_sat %d %f", k, values[k]);
+            k++;
         }
         // 0 + 7 == red
         // 4 + 5 == blue
-       int total = 0;
+       double total = 0.0;
 
         for ( int i = 0; i<BINS; i++){
 
             total+=values[i];
+            RobotLog.dd(TAG, "Total %4.2f", total);
         }
-        double red = values[0] + values[7];
+        double red =  values[0] + values[7];
         double blue = values[4] + values[5];
         double redp = red/total;
         double bluep = blue/total;
-        if(bluep >= THRESHOLD){
+        redPct = redp;
+        bluPct = bluep;
+
+        //UHHH - WHY does this work with bluep and redp what seems to be reversed?
+        if(redp >= THRESHOLD){
             //yayRed!!! :D
             foundColor = Color.RED;
         }
-        else if(redp >= THRESHOLD)
+        else if(bluep >= THRESHOLD)
         {
             //yayBlue!!! :D
             foundColor = Color.BLUE;
@@ -88,7 +132,5 @@ public class MajorColorDetector extends Detector {
             //booNone!!!:(
             foundColor = Color.NONE;
         }
-
-
     }
 }
