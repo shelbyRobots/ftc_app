46197bd73a24f08c83e2f888f77ec48f8492a96d
shelbyRobots <haithcockfamily@gmail.com>
2018-09-19 23:43:56 -0400

    Rover Ruckus Field points - First class for the year.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/field/RoRuField.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode.field;
+
+
+import org.firstinspires.ftc.teamcode.util.Point2d;
+
+
+
+@SuppressWarnings({"unused", "WeakerAccess"})
+public class RoRuField extends Field
+{
+    //Point naming key:
+    //1st char: R=Red, B=Blue
+    //2nd char: L=Left start, R=Right start (viewed from red side - along field X)
+    //3rd-4th chars:
+
+
+    private static final String TAG = " SJH_RFD";
+
+
+    private static final int BLUE  = 0;
+    private static final int RED   = 1;
+    private static final int STRT1 = 0;
+    private static final int STRT2 = 1;
+    private static final int LEFT  = 0;
+    private static final int CNTR  = 1;
+    private static final int RGHT  = 2;
+
+
+    static void initField(String robotName, double gOffset)
+    {
+
+    }
+
+
+
+    //Red Route > Left
+    static final Point2d RLLP = new Point2d("RLLP", -8.25,  -8.25);
+    static final Point2d RLTP = new Point2d("RLTP", -17.25, -17.25);
+    static final Point2d RLM1 = new Point2d("RLM1", -48.0,  -24.0);
+    static final Point2d RLM2 = new Point2d("RLM2", -36.0,  -36.0);
+    static final Point2d RLM3 = new Point2d("RLM3", -24.0,  -48.0);
+    static final Point2d RLDT = new Point2d("RLDT",   0.0,  -52.0);
+    static final Point2d RLDP = new Point2d("RLDP",  42.0,  -55.0);
+    static final Point2d RLPP = new Point2d("RLPP", -24.0,  -55.0);
+
+    static final Point2d RBLP = new Point2d("BLLP",  8.25,  8.25);
+    static final Point2d RBTP = new Point2d("BLTP",  17.25, 17.25);
+    static final Point2d RBM1 = new Point2d("BLM1",  48.0,  24.0);
+    static final Point2d RBM2 = new Point2d("BLM2",  36.0,  36.0);
+    static final Point2d RBM3 = new Point2d("BLM3",  24.0,  48.0);
+    static final Point2d RBDT = new Point2d("BLDT",   0.0,  52.0);
+    static final Point2d RBDP = new Point2d("BLDP", -42.0,  55.0);
+    static final Point2d RBPP = new Point2d("BLPP",  24.0,  55.0);
+
+    static final Point2d RRLP = new Point2d("RRLP",  8.25,  -8.25);
+    static final Point2d RRTP = new Point2d("RRTP",  17.25, -17.25);
+    static final Point2d RRM3 = new Point2d("RRM3",  48.0,  -24.0);
+    static final Point2d RRM2 = new Point2d("RRM2",  36.0,  -36.0);
+    static final Point2d RRM1 = new Point2d("RRM1",  24.0,  -48.0);
+    static final Point2d RRDT = new Point2d("RRDT",   52.0,  0.0);
+    static final Point2d RRDP = new Point2d("RRDP",  55.0,  -42.0);
+    static final Point2d RRPP = new Point2d("RRPP", 55.0,  24.0);
+
+    static final Point2d BRLP = new Point2d("BRLP",  -8.25,   8.25);
+    static final Point2d BRTP = new Point2d("BRTP",  -17.25,  17.25);
+    static final Point2d BRM3 = new Point2d("BRM3",  -48.0,   24.0);
+    static final Point2d BRM2 = new Point2d("BRM2",  -36.0,   36.0);
+    static final Point2d BRM1 = new Point2d("BRM1",  -24.0,   48.0);
+    static final Point2d BRDT = new Point2d("BRDT",  -52.0,  0.0);
+    static final Point2d BRDP = new Point2d("BRDP",  -55,   42.0);
+    static final Point2d BRPP = new Point2d("BRPP", -55.0,  -24.0);
+
+}
